c5273eb7992d9f1c884ef2fb3bbee69956da05fd
Tatyana <tatyanalipch@yandex.ru>
2024-05-09 21:41:41 +0300

    src/

--- src/test/java/ru/netology/services/rascetotpuska/VacationCalculationTest.java
+++ src/test/java/ru/netology/services/rascetotpuska/VacationCalculationTest.java
@@ -2,17 +2,25 @@ package ru.netology.services.rascetotpuska;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationCalculationTest {
 
-    @Test
-    public void testSmallThresholdMoney() {
+    @ParameterizedTest
+   // @CsvSource({
+   //         "10000, 3000, 20000, 3",
+  //          "100000, 60000, 150000, 2"
+  //  })
+    @CsvFileSource(files="src/test/resources/values.csv")
+    public void testSmallThresholdMoney(int income, int expenses, int threshold, int expected ) {
         VacationCalculation service = new VacationCalculation();
 
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
+      //  int income = 10_000;
+      //  int expenses = 3_000;
+      //  int threshold = 20_000;
+     //   int expected = 3;
 
         int actual = service.calculate(income, expenses, threshold);
 
@@ -21,19 +29,19 @@ public class VacationCalculationTest {
     }
 
 
-    @Test
-    public void testMaximumThresholdMoney() {
-        VacationCalculation service = new VacationCalculation();
+  //  @Test
+   // public void testMaximumThresholdMoney() {
+  //      VacationCalculation service = new VacationCalculation();
 
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
+  //      int income = 100_000;
+  //      int expenses = 60_000;
+   //     int threshold = 150_000;
+  //      int expected = 2;
 
-        int actual = service.calculate(income, expenses, threshold);
+ //       int actual = service.calculate(income, expenses, threshold);
 
 
-        Assertions.assertEquals(expected, actual);
-    }
+   //     Assertions.assertEquals(expected, actual);
+   // }
 }
 
